944d5133e5e983152bf640fd4f097f5ab6ed2f60
amina jandoubi <133951150+aminajandoubi@users.noreply.github.com>
2024-02-17 16:57:58 +0100

    Create device.py

---
+++ Execution with Mosquitto/Scénario 2-Replay Attack/message connect/device.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import time
+broker="localhost"
+port =1883
+mqttv=mqtt.MQTTv5
+client_id="publisher"
+
+def on_log(client, userdata, level, buf):
+   print(buf) 
+
+def on_connect(client, userdata, flags, rc, protocol):
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+        client.loop_stop()  
+
+mqtt.Client.connected_flag=False#create flag in class
+
+client = mqtt.Client("publisher",protocol=mqttv)             #create new instance
+
+client.username_pw_set(username="publisher",password="123456")
+
+client.on_log=on_log
+
+client.on_connect = on_connect
+
+client.connect(broker,port)           #establish connection
+
+client.loop_start()
+
+while not client.connected_flag: #wait in loop
+    print("In wait loop")
+    time.sleep(100)
+
+client.loop_stop()
